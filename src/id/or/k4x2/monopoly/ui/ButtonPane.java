d3948179e21fab7a2d27d46d0bf6e9aaf2aa3d66
didithilmy <didithilmy@gmail.com>
2019-03-29 17:36:07 +0700

    Added button pane

---
+++ src/id/or/k4x2/monopoly/ui/ButtonPane.java
@@ -0,0 +1,31 @@
+package id.or.k4x2.monopoly.ui;
+
+import javax.swing.*;
+
+public class ButtonPane {
+    private JPanel panel;
+    private JButton rollDiceButton;
+    private JButton endTurnButton;
+    private JButton buyLotButton;
+    private JButton buildAHouseButton;
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public JButton getRollDiceButton() {
+        return rollDiceButton;
+    }
+
+    public JButton getEndTurnButton() {
+        return endTurnButton;
+    }
+
+    public JButton getBuyLotButton() {
+        return buyLotButton;
+    }
+
+    public JButton getBuildAHouseButton() {
+        return buildAHouseButton;
+    }
+}

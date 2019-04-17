68294f2f2b0dcb534d4b5bc005720caf63302a26
yanzahatta <yanza.hatta@gmail.com>
2019-04-17 17:56:57 +0700

    by form dan by java

---
+++ src/main/java/id/or/k4x2/monopoly/ui/By.java
@@ -0,0 +1,30 @@
+package id.or.k4x2.monopoly.ui;
+
+import javax.swing.*;
+
+public class By {
+    private JPanel panel1;
+    private JPanel NamaNim;
+    private JLabel Player;
+    private JFrame frame;
+
+    public By (String nama){
+        Player.setText(nama);
+    }
+
+    public JPanel getPanel(){
+        return panel1;
+    }
+
+    public void show() {
+        frame = new JFrame("Winner");
+
+        frame.setContentPane(getPanel());
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+    }
+
+}
